16f3a624f121487ae7d726849af9bfd35ae17783
sd2017 <sd2017@walla.co.il>
2016-10-24 14:03:52 +0300

    initial for clion

---
+++ docker-cl201623/01/runner.sh
@@ -0,0 +1,29 @@
+docker run --name=clion23x -it \
+    --rm \
+    --user=developer \
+    --env="DISPLAY" \
+    --workdir="/home/developer" \
+    --volume="/home/developer:/home/developer" \
+    --volume="/etc/group:/etc/group:ro" \
+    --volume="/etc/passwd:/etc/passwd:ro" \
+    --volume="/etc/shadow:/etc/shadow:ro" \
+    --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    sd2017/clion23:01
+
+xhost +local:`docker inspect --format='{{ .Config.Hostname }}' clion23x`
+
+#    XSOCK=/tmp/.X11-unix
+#    XAUTH=/tmp/.docker.xauth
+#    touch $XAUTH
+#    xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+#    docker run -it \
+#            --workdir="/home/developer" \
+#            --volume="/home/developer:/home/developer" \
+#            --volume=$XSOCK:$XSOCK:rw \
+#            --volume=$XAUTH:$XAUTH:rw \
+#            --env="XAUTHORITY=${XAUTH}" \
+#            --env="DISPLAY" \
+#            --user="developer" \
+#        sd2017/ubuntu:06
